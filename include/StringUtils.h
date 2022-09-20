9c725b79730f98a4958937b7309adda1c3c3b0a2
Jeffrey Strom <jstrom2002@hotmail.com>
2022-09-20 02:02:32 -0600

    Initial commit

---
+++ include/StringUtils.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "stdafx.h"
+#include <string>
+#include <vector>
+
+namespace PointcloudVisualizer
+{
+	std::vector<std::string> tokenize(std::string toTokenize, std::string token);	
+}
